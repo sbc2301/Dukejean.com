e4277a51685244a1181af39718045f252608c8e1
Sean Clifford <scliffo2@its.jnj.com>
2025-02-02 17:40:19 -0500

    duke jean logo

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import arm from './arm.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={arm} className="Record-Arm" alt="dukearm"/>
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Dukejean: <code>I am Duke JEAN</code> and deadpool
+          Dukejean: <code>I am fart JEAN</code> and deadpool
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=CXgU8ghjTx8"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Fart
         </a>
       </header>
     </div>
