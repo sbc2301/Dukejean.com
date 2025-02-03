46dea76809a4b31b4120d9cf77a04adf809c4060
Sean Clifford <scliffo2@its.jnj.com>
2025-02-02 20:17:08 -0500

    add duke jean

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import arm from './arm.svg'
 import './App.css';
 
 function App() {
+
+  const [text, setText] = useState("I AM DUKE JEAN");
+
+  function Balls() {
+    setText((prev) => (prev === "I AM DUKE JEAN" ? "I DUKE" : "I AM DUKE JEAN"));
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-      <img src={arm} className="Record-Arm" alt="dukearm"/>
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Dukejean: <code>I am fart JEAN</code> and deadpool
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=CXgU8ghjTx8"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Fart
-        </a>
+        <div className="PenisMunch">
+          
+        <img src={arm} className="Record-Arm" alt="dukearm"/>
+        <img src={logo} className="App-logo" alt="logo" onClick={Balls} />
+        <div>{text}</div>
+        </div>
       </header>
+
     </div>
   );
 }
